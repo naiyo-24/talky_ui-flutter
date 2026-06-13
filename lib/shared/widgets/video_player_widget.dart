bac2e0ae53609abe5c9305780c0ffa5a92fd1052
Mudassar2107 <bwahida168@gmail.com>
2026-06-14 00:35:39 +0530

    refactor: disable video player functionality and remove dependencies

--- lib/shared/widgets/video_player_widget.dart
+++ lib/shared/widgets/video_player_widget.dart
@@ -1,117 +1,23 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'full_screen_video_player.dart';
 
-class VideoPlayerWidget extends StatefulWidget {
+class VideoPlayerWidget extends StatelessWidget {
   final String videoPath;
 
   const VideoPlayerWidget({super.key, required this.videoPath});
 
   @override
-  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
-}
-
-class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late VideoPlayerController _controller;
-  bool _isMuted = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.file(File(widget.videoPath))
-      ..initialize().then((_) {
-        setState(() {});
-        _controller.setVolume(0.0); // Muted by default
-        _controller.setLooping(true);
-        _controller.play(); // Auto-play
-      });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _toggleMute() {
-    setState(() {
-      _isMuted = !_isMuted;
-      _controller.setVolume(_isMuted ? 0.0 : 1.0);
-    });
-  }
-
-  void _openFullScreen() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => FullScreenVideoPlayer(videoPath: widget.videoPath),
-      ),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (!_controller.value.isInitialized) {
-      return Container(
-        height: 200,
-        width: double.infinity,
+    return Container(
+      height: 200,
+      width: double.infinity,
+      decoration: BoxDecoration(
         color: Colors.black12,
-        child: const Center(child: CircularProgressIndicator()),
-      );
-    }
-
-    return GestureDetector(
-      onTap: _openFullScreen, // Tap anywhere to open full screen
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.black,
-        ),
-        clipBehavior: Clip.hardEdge,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            ),
-            // Mute/Unmute button
-            Positioned(
-              bottom: 8,
-              right: 8,
-              child: GestureDetector(
-                onTap: _toggleMute,
-                child: Container(
-                  padding: const EdgeInsets.all(6),
-                  decoration: const BoxDecoration(
-                    color: Colors.black54,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    _isMuted ? Icons.volume_off : Icons.volume_up,
-                    color: Colors.white,
-                    size: 20,
-                  ),
-                ),
-              ),
-            ),
-            // Play overlay icon to indicate it's a video
-            Positioned(
-              child: Container(
-                padding: const EdgeInsets.all(12),
-                decoration: const BoxDecoration(
-                  color: Colors.black45,
-                  shape: BoxShape.circle,
-                ),
-                child: const Icon(
-                  Icons.play_arrow,
-                  color: Colors.white,
-                  size: 32,
-                ),
-              ),
-            ),
-          ],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: const Center(
+        child: Text(
+          'Video feature temporarily disabled',
+          style: TextStyle(color: Colors.white54),
         ),
       ),
     );
